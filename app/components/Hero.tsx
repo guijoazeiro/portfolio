d3f60681867999a1a63c9e7135a9cf343d9b39ef
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-06-05 15:23:35 -0300

    add typewritter

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,19 @@
+import { Typewriter } from "nextjs-simple-typewriter";
+
 const Hero = () => {
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
       <div className="w-full md:w-1/2 text-center p-5 md:p-0">
         <h1 className="mb-5 md:text-5xl text-3xl font-bold">
-         Olá, sou <span className="text-[#93DEFF]">Guilherme Joazeiro</span>
+          Olá, sou{" "}
+          <span className="text-[#93DEFF]">
+            <Typewriter
+              words={[" Guilherme Joazeiro", " Desenvolvedor Backend"]}
+              cursor={true}
+              loop={0}
+            />
+          </span>
         </h1>
-        
       </div>
     </div>
   );
