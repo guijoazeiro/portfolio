ec029aa8d2a6f652467d0a6f8dc7126e45f57224
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-06-27 16:28:12 -0300

    hotfix: remove download cv button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -18,14 +18,14 @@ const Hero = () => {
           </span>
         </h1>
         <div className="flex justify-center space-x-4 relative">
-          <a
+          {/* <a
             href={`/${t("cvlink")}`}
             download
             className="px-4 py-2 bg-gradient-to-r from-[#00c6ff] to-[#0072ff] text-white rounded-3xl mt-16 inline-flex items-center cursor-pointer transition-all duration-100 ease-in transform hover:scale-105"
           >
             <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
             {t("cv")}
-          </a>
+          </a> */}
           <a
             href="#contact"
             className="px-4 py-2 bg-zinc-800 border-gray-800 rounded-3xl text-white inline-block mt-16 cursor-pointer transition-all duration-100 ease-in transform hover:scale-105"
