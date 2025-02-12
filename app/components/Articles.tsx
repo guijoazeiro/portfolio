4ddc33ffac80a831f7f624dd2b0eed741b5a6359
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2025-02-12 16:26:07 -0300

    add article

--- app/components/Articles.tsx
+++ app/components/Articles.tsx
@@ -1,20 +1,23 @@
-import Link from 'next/link';
-import { FiLinkedin } from 'react-icons/fi';
-import { useTranslations } from 'next-intl';
+import Link from "next/link";
+import { FiLinkedin } from "react-icons/fi";
+import { useTranslations } from "next-intl";
 
 const Articles = () => {
   const t = useTranslations("Articles");
-  const keys = ["First", "Second",] as const;
+  const keys = ["First", "Second", "Third"] as const;
 
   return (
     <div className="container mx-auto px-5 pb-10 md:px-0" id="articles">
       <h1 className="underline underline-offset-4 mb-5 text-2xl">{t("h1")}</h1>
 
       <div className="flex items-center justify-between flex-wrap">
-        {keys.map((article) => {
+        {keys.map((article, index) => {
+          const isLast = index === keys.length - 1 && keys.length % 2 !== 0;
           return (
             <div
-              className="bg-zinc-800 bg-opacity-20 hover:border-gray-600 transition-all duration-500 border-gray-800 border rounded-md p-5 w-full md:w-[49%] flex flex-col justify-between items-center mb-5 gap-10"
+              className={`bg-zinc-800 bg-opacity-20 hover:border-gray-600 transition-all duration-500 border-gray-800 border rounded-md p-5 w-full md:w-[49%] flex flex-col justify-between items-center mb-5 gap-10 ${
+                isLast ? "md:mx-auto" : ""
+              }`}
               key={t(`${article}.id`)}
             >
               <div className="w-full flex items-center justify-end gap-5">
@@ -31,8 +34,7 @@ const Articles = () => {
                 {t(`${article}.description`)}
               </p>
 
-              <div className="flex gap-3 flex-wrap items-center justify-center">
-              </div>
+              <div className="flex gap-3 flex-wrap items-center justify-center"></div>
             </div>
           );
         })}
