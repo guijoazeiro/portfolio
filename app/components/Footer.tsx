3bfdc7a5732ba1f34fa76dcc070dfc24292d7e7c
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2026-08-04 16:38:08 -0300

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-16 pt-4 border-t border-gray-800 text-xs text-gray-500">
-      &copy; 2024 Guilherme Joazeiro
+      &copy; 2026 Guilherme Joazeiro
     </footer>
   );
 };
